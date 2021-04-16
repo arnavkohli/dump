d455503f55ad60a1de32d8424b7e20eb756bbf52
Arnav Kohli <arnavkohli@gmail.com>
2021-04-16 14:44:01 +0530

    🛠️  bug fixes; added requirments.txt

--- git/github_laziness.py
+++ git/github_laziness.py
@@ -11,20 +11,24 @@ def time_that_shit(func):
 	return not_good_with_names
 
 # Helper functions
-def clone_and_filter_branch(URL):
+def clone_and_filter_branch(url, git_author_name, git_author_email, git_committer_name, git_committer_email):
 	'''
 		Clones the repo locally, filters the branch, pushes the changes, deletes the cloned dir.
 	'''
-	NAME = URL.split('/')[-1].split('.')[0]
+	NAME = url.split('/')[-1].split('.')[0]
 
 	# Clone the repo
-	os.system(f"git clone {URL}")
+	os.system(f"git clone {url}")
 
 	# cd inside dir
 	os.chdir(NAME)
 
 	# filter the branch and push the changes
-	os.system(f'''git filter-branch -f --env-filter "GIT_AUTHOR_NAME='{GIT_AUTHOR_NAME}'; GIT_AUTHOR_EMAIL='{GIT_AUTHOR_EMAIL}'; GIT_COMMITTER_NAME='{GIT_COMMITTER_NAME}'; GIT_COMMITTER_EMAIL='{GIT_COMMITTER_EMAIL}';"''')
+	os.system(f'''git filter-branch -f --env-filter "GIT_AUTHOR_NAME='{git_author_name}'; \
+		GIT_AUTHOR_EMAIL='{git_author_email}'; \
+		GIT_COMMITTER_NAME='{git_committer_name}'; \
+		GIT_COMMITTER_EMAIL='{git_committer_email}';"''')
+
 	os.system("git push --force --tags origin HEAD:master")
 
 	# cd back to parent dir and rm cloned repo
@@ -33,45 +37,60 @@ def clone_and_filter_branch(URL):
 
 # Mains
 @time_that_shit
-def update_author_and_committer(repo_names, github_username):
+def update_author_and_committer(repo_names, github_info):
 	'''
-		Constructs clone URLs for each repo and updates them.
+		Constructs clone urls for each repo and updates them.
 	'''
-	urls = [f"https://github.com/{GITHUB_USERNAME}/{repo_name}" for repo_name in repo_names]
-	for index, URL in enumerate(urls):
+	urls = [f"https://github.com/{github_info.get('GITHUB_USERNAME')}/{repo_name}" for repo_name in repo_names]
+	for index, url in enumerate(urls):
 		print (f"[{index + 1} / {len(urls)}]")
-		clone_and_filter_branch(URL)
+		clone_and_filter_branch(
+			url=url,
+			git_author_name=github_info.get('GIT_AUTHOR_NAME'),
+			git_author_email=github_info.get('GIT_AUTHOR_EMAIL'),
+			git_committer_name=github_info.get('GIT_COMMITTER_NAME'),
+			git_committer_email=github_info.get('GIT_COMMITTER_EMAIL')
+		)
 
 	print (f"[INFO] update_author_and_committer has finished running")
 
 @time_that_shit
-def update_author_and_committer_for_public_repos(github_username):
+def update_author_and_committer_for_public_repos(github_info):
 	'''
-		Scrapes clone URLs of the public repos (fork=False) of the user and updates them.
+		Scrapes clone urls of the public repos (fork=False) of the user and updates them.
 	'''
-	URL = f"https://api.github.com/users/{github_username}/repos"
-	all_repos = requests.get(URL).json()
+	url = f"https://api.github.com/users/{github_info.get('GITHUB_USERNAME')}/repos"
+	all_repos = requests.get(url).json()
 
 	count = 0
 	for index, repo in enumerate(all_repos):
+		print (repo)
 		if not repo.get('fork'):
 			clone_url = repo.get("clone_url")
 			print ("[***********] Starting", clone_url)
-			clone_and_filter_branch(clone_url)
+			clone_and_filter_branch(
+				url=clone_url,
+				git_author_name=github_info.get('GIT_AUTHOR_NAME'),
+				git_author_email=github_info.get('GIT_AUTHOR_EMAIL'),
+				git_committer_name=github_info.get('GIT_COMMITTER_NAME'),
+				git_committer_email=github_info.get('GIT_COMMITTER_EMAIL')
+			)
 			print (f"[***********] Count: {count + 1} / {len(all_repos)}")
 			count += 1
 
 	print (f"[INFO] update_author_and_committer_for_public_repos has finished running")
 
 if __name__ == '__main__':
-	GIT_AUTHOR_NAME     = ""
-	GIT_AUTHOR_EMAIL    = ""
-	GIT_COMMITTER_NAME  = ""
-	GIT_COMMITTER_EMAIL = ""
-	GITHUB_USERNAME = ""
+	GITHUB_INFO = {
+		"GITHUB_USERNAME" : "arnavkohli",
+		"GIT_AUTHOR_NAME"     : "Arnav Kohli",
+		"GIT_AUTHOR_EMAIL"    : "arnavkohli@gmail.com",
+		"GIT_COMMITTER_NAME"  : "Arnav Kohli",
+		"GIT_COMMITTER_EMAIL" : "arnavkohli@gmail.com"
+	}
 	REPO_NAMES = [
-		GITHUB_USERNAME
+		"QuizApp"
 	]
-	
-	# update_author_and_committer(repo_names=REPO_NAMES, github_username=GITHUB_USERNAME)
-	# update_author_and_committer_for_public_repos(github_username=GITHUB_USERNAME)
+
+	update_author_and_committer(REPO_NAMES, GITHUB_INFO)
+	#update_author_and_committer_for_public_repos(GITHUB_INFO)
